fa3deabb4d219cbed53bebc00895aca9cb218e4c
Joshua Rully <joshrully@gmail.com>
2024-08-06 22:15:47 -0700

    [main] Begin using best practices; shake off the rustiness of rust

--- hour_clock/scs_hour_clock/src/main.rs
+++ hour_clock/scs_hour_clock/src/main.rs
@@ -1,9 +1,3 @@
-// CLI program?
-fn main() {
-    let twelve = Hours::Twelve;
-    println!("The hour is {:?}", increment_hours(twelve));
-}
-
 #[derive(Debug)]
 enum Hours {
     One,
@@ -19,23 +13,38 @@ enum Hours {
     Eleven,
     Twelve,
 }
+struct HoursClock {
+    hour: Hours,
+}
 
-// TODO: refactor to impl; handle errors?
-fn increment_hours(hr: Hours) -> Hours {
-    match hr {
-        Hours::One => Hours::Two,
-        Hours::Two => Hours::Three,
-        Hours::Three => Hours::Four,
-        Hours::Four => Hours::Five,
-        Hours::Five => Hours::Six,
-        Hours::Six => Hours::Seven,
-        Hours::Seven => Hours::Eight,
-        Hours::Eight => Hours::Nine,
-        Hours::Nine => Hours::Ten,
-        Hours::Ten => Hours::Eleven,
-        Hours::Eleven => Hours::Twelve,
-        Hours::Twelve => Hours::One,
+impl HoursClock {
+    fn new() -> Self {
+        HoursClock { hour: Hours::Twelve}
     }
+
+    fn increment_hour(&self) -> Hours {
+        match self.hour {
+            Hours::One => Hours::Two,
+            Hours::Two => Hours::Three,
+            Hours::Three => Hours::Four,
+            Hours::Four => Hours::Five,
+            Hours::Five => Hours::Six,
+            Hours::Six => Hours::Seven,
+            Hours::Seven => Hours::Eight,
+            Hours::Eight => Hours::Nine,
+            Hours::Nine => Hours::Ten,
+            Hours::Ten => Hours::Eleven,
+            Hours::Eleven => Hours::Twelve,
+            Hours::Twelve => Hours::One,
+        }
+    }
+}
+
+
+// CLI program?
+fn main() {
+    let hours_clock = HoursClock::new();
+    println!("The hour is {:?}", hours_clock.increment_hour());
 }
 
 // TODO: write tests :)
